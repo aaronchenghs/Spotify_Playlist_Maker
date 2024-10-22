303f60150538c18f26e814ecf7aad291e5133d3d
Aaron Cheng <acheng8@lsu.edu>
2024-10-21 21:12:38 -0500

    Workflow beginnings

--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -1,16 +1,86 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import "./_Home.styles.css";
 import { CopyFooter } from "../Common/CopyFooter/CopyFooter";
+import MainTextTypography from "../Common/MainTextTypography/MainTextTypography";
+
+const CreateSteps: React.ReactNode[] = [];
+const FilterSteps: React.ReactNode[] = [];
+
+type Choice = "create" | "filter";
 
 const Home: React.FC = () => {
-  return (
-    <div className="homeContainer">
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the home page of the BPM App.</p>
+  // Animation state
+  const [contentHeight, setContentHeight] = useState("85vh");
+  const [footerVisible, setFooterVisible] = useState(true);
+  // Step State
+  const [choice, setChoice] = useState<Choice>("create");
+  const [step, setStep] = useState(0);
 
+  useEffect(() => {
+    const timeoutHeight = setTimeout(() => {
+      setContentHeight("0px");
+    }, 50);
+
+    const timeoutFooter = setTimeout(() => {
+      setFooterVisible(false);
+    }, 700);
+
+    return () => {
+      clearTimeout(timeoutHeight);
+      clearTimeout(timeoutFooter);
+    };
+  }, []);
+
+  const handleChoice = (choice: Choice) => {
+    setChoice(choice);
+    setStep(1);
+  };
+
+  const content = useMemo(() => {
+    console.log("HELLO");
+    if (step === 0) {
+      return <ChooseContent onChoose={handleChoice} />;
+    }
+
+    return choice === "create" ? CreateSteps[step] : FilterSteps[step];
+  }, [step, choice]);
+
+  const initialContent = (
+    <div
+      className={`initialHomeContainer ${
+        footerVisible ? "fade-in" : "fade-out"
+      } `}
+    >
+      <div className="initialContent" style={{ height: contentHeight }}></div>
       {CopyFooter}
     </div>
+  );
+
+  return footerVisible ? (
+    initialContent
+  ) : (
+    <div className="homeContainer">{content}</div>
   );
 };
 
 export default Home;
+
+interface ChooseContentProps {
+  onChoose: (choice: Choice) => void;
+}
+
+const ChooseContent: React.FC<ChooseContentProps> = ({ onChoose }) => {
+  return (
+    <div className="step0Container">
+      <button onClick={() => onChoose("create")}>Create a New Playlist</button>
+      <div className="divider">
+        <MainTextTypography tag="h3">
+          <i>What would you like to do?</i>
+        </MainTextTypography>
+      </div>
+      <button onClick={() => onChoose("filter")}>
+        Filter an Existing Playlist
+      </button>
+    </div>
+  );
+};
--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -14,31 +14,33 @@ const Landing: React.FC = () => {
 
   return (
     <div className="landing-container">
-      <header className="landing-header">
-        <MainTextTypography accent tag="h1">
-          {AppName}
-        </MainTextTypography>
-        <p>A quicker way to make your Spotify playlists</p>
-      </header>
-      <main className="landing-main">
-        <section className="features">
-          <ul>
-            <li>Create playlists by BPM.</li>
-            <li>Filter your existing playlists by tempo.</li>
-            <li>Automatically sync songs from recent favorites.</li>
-          </ul>
-        </section>
-        <SpotifyButton onClick={handleSpotifyLogin}>
-          <MainTextTypography>
-            Log in with Spotify{" "}
-            <img
-              src={SpotifyLogo}
-              alt="Spotify Logo"
-              className="spotify-logo"
-            />
+      <div className="main-content">
+        <header className="landing-header">
+          <MainTextTypography accent tag="h1">
+            {AppName}
           </MainTextTypography>
-        </SpotifyButton>
-      </main>
+          <p>A quicker way to make your Spotify playlists</p>
+        </header>
+        <main className="landing-main">
+          <section className="features">
+            <ul>
+              <li>Create playlists by BPM.</li>
+              <li>Filter your existing playlists by tempo.</li>
+              <li>Automatically sync songs from recent favorites.</li>
+            </ul>
+          </section>
+          <SpotifyButton onClick={handleSpotifyLogin}>
+            <MainTextTypography>
+              Log in with Spotify{" "}
+              <img
+                src={SpotifyLogo}
+                alt="Spotify Logo"
+                className="spotify-logo"
+              />
+            </MainTextTypography>
+          </SpotifyButton>
+        </main>
+      </div>
       {CopyFooter}
     </div>
   );
